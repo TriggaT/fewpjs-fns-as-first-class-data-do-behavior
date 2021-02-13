ab6a58baccce1d9eef55dedc57a92c431b3a3a04
Trayshawn <traytwebb14@yahoo.com>
2021-02-13 01:31:43 -0600

    commit tree ef0f66a1e090594988ad18af5596665ade831785

--- index.js
+++ index.js
@@ -9,5 +9,21 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time){
+  const t = parseInt(time)
+  if (t < 12){
+    return "Good Morning"
+  }
+  if (t >= 12 && t <= 17){
+    return "Good Afternoon"
+  } 
+  if (t > 17 && t < 24){
+    return "Good Evening"
+  }
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg
+}
